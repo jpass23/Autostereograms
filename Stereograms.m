b9373988f551f619e4e3d10ab3eafd99196d608b
jpass23 <jpass23@live.unc.edu>
2022-11-21 15:37:00 -0500

    Made a stereogram
    But it only kind of works

--- Stereograms.m
+++ Stereograms.m
@@ -2,14 +2,30 @@
 %     
 % end
 
-I = imread("pattern.png");
-repeat = repeatIm(I,6);
-shifted = imtranslate(repeat, [10, 0]);
-imshow(shifted)
+pattern = imread("pattern.png");
+I = 256.*imread("Test.png");
 
-function repeated = repeatIm(im,repVal)
-    repeated = zeros(size(im,1),repVal*size(im,2),size(im,3));
+repeat = repeatIm(pattern,6);
+gram = stereogram(repeat,I,20);
+%figure; imshow(repeat);
+figure; imshow(gram);
+
+function autoGram = stereogram(pattern,im,shamt)
+    im = im2gray(im);
+    autoGram = pattern;
+    shifted = imtranslate(pattern, [shamt, 0]);
+    for i = 1:size(im,1)
+        for j = 1:size(im,2)
+            if im(i,j) >= 250
+                autoGram(i,j) = shifted(i,j);
+            end
+        end
+    end
+end
+
+function repeated = repeatIm(pattern,repVal)
+    repeated = zeros(size(pattern,1),repVal*size(pattern,2),size(pattern,3));
     for i = 1:repVal
-        repeated(:,(i-1)*size(im,2)+1:i*size(im,2),:) = im2double(im);
+        repeated(:,(i-1)*size(pattern,2)+1:i*size(pattern,2),:) = im2double(pattern);
     end
 end
