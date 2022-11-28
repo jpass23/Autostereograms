22c2c8c397564ffff80c6b6a58fd31b657c2526d
turner-white <turnerwhite@me.com>
2022-11-28 14:27:58 -0500

    yay
    it actually works

--- Stereograms.m
+++ Stereograms.m
@@ -1,8 +1,13 @@
-im = imread("Test.png");
-pattern = imread("pattern.png");
-im2 = imread("Test2.jpeg");
+im = imread("squirrel.jpeg");
+pattern = imread("pattern2.jpeg");
 depthBruh = edgeDetect(im2);
-imshow(depthBruh);
+fullPattern = patternCast(im,pattern);
+gram = stereogram(im,fullPattern,20);
+figure(3);imshow(fullPattern)
+figure(4);imshow(gram)
+animate(fullPattern,gram);
+
+
 
 %function depthMap = outline(im)
 function depthMap = edgeDetect(im)
@@ -12,7 +17,7 @@ function depthMap = edgeDetect(im)
     gradX = double(imfilter(blurred,fspecial('sobel').'));
 
     magnitude = sqrt(gradY.^2 + gradX.^2);
-    threshold = prctile(magnitude,97,"all");
+    threshold = prctile(magnitude,90,"all");
 
     edges = double(magnitude > threshold);
     depthMap = edges;
@@ -21,41 +26,42 @@ end
 %function pattern = stretch(depthMap, pattern)
 %function fullPattern = patternCast(depthMap, pattern)
 function fullPattern = patternCast(depthMap, pattern)
-    height, width = size(depthMap);
-    patH, patW = size(pattern);
-    fullPattern = zeros(height,width);
+    height = size(depthMap,1);
+    width = size(depthMap,2);
+    patH = size(pattern,1);
+    patW = size(pattern,2);
+    fullPattern = zeros(height,width,3);
     for i=1:height
         for j=1:width
             iMod = mod(i,patH) + 1;
             jMod = mod(j,patW) + 1;
-            fullPattern(i,j) = pattern (iMod, jMod);
+            fullPattern(i,j,:) = pattern(iMod, jMod,:);
         end
-    end    
+    end
+    fullPattern = imfilter(fullPattern,fspecial("gaussian",6,1));
+    fullPattern = uint8(fullPattern);
 end
 %function autoGram = stereogram(depthMap, fullPattern)
 function autoGram = stereogram(depthMap, fullPattern, shiftMult)
-    depthNorm = depthMap ./ max(depthMap(:));
-    height,width = size(depthMap);
+    depthNorm = double(im2gray(depthMap));
+    depthNorm = depthNorm ./ max(depthNorm(:));
+    height = size(depthMap,1);
+    width = size(depthMap,2);
     autoGram = fullPattern;
      for i=1:height
         for j=1:width
-            iShift = i + (depthNorm(i,j) * shiftMult);
-            autoGram(i,j) = fullPattern(iShift,j);
+            jShift = j + floor(depthNorm(i,j) * shiftMult);
+            autoGram(i,j,:) = fullPattern(i,jShift,:);
         end
-    end    
+     end
+    autoGram = imfilter(autoGram,fspecial("gaussian",3,1));
 end
 %function animated = animate(fullPattern, autoGram)
-function animated = animate(fullPattern,autoGram)
+function animate(fullPattern,autoGram)
     filename = "testAnimated.gif"; % Specify the output file name
     [A1,map1] = rgb2ind(fullPattern,256);
     [A2,map2] = rgb2ind(autoGram,256);
-    imwrite(A1,map1,filname,"gif","LoopCount",Inf,"DelayTime",0.1);
+    imwrite(A1,map1,filename,"gif","LoopCount",Inf,"DelayTime",0.1);
     imwrite(A2,map2,filename,"gif","WriteMode","append","DelayTime",0.1);
-end
 
-function repeated = repeatIm(pattern,repVal)
-    repeated = uint8(zeros(size(pattern,1),repVal*size(pattern,2),size(pattern,3)));
-    for i = 1:repVal
-        repeated(:,(i-1)*size(pattern,2)+1:i*size(pattern,2),:) = im2double(pattern);
-    end
 end
