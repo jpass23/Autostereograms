215a355c9bc244ebe26d87db8ea1be6183123d27
turner-white <turnerwhite@me.com>
2022-11-30 21:44:50 -0500

    finally it works

--- Stereograms.m
+++ Stereograms.m
@@ -1,30 +1,39 @@
-im = imread("squirrel.jpeg");
-pattern = imread("pattern2.jpeg");
-im2 = imread("face.jpeg");
-
+%-------------------------------------
 word = {'B', 'R', 'U', 'H'};
 s=4;
-%-------------------------------------
 wordPattern = im2double(imread("wordPattern.jpeg"));
 wordDepthMap = makeWordDepthMap(word,s);
+imwrite(wordDepthMap,"wordDepthMap.jpeg")
 %figure(2); imshow(wordDepthMap);
 wordFullPattern = patternCast(wordDepthMap,wordPattern);
+imwrite(wordFullPattern,"wordFullPattern.jpeg")
 wordGram = stereogram(wordDepthMap,wordFullPattern,15,2);
-figure(1); imshow(wordGram);
+%figure(1); imshow(wordGram);
 animate(imgaussfilt(wordFullPattern,2),wordGram,"wordGif.gif");
 
 
-%--------------------------------------
-newPattern = compress(im, im2, 10);
+%-----------------------------------------
+map1 = imread("squirrel.jpeg");
+custIm = imread("jaden.jpeg");
+custIm = imresize(custIm,0.125);
 
+newPattern = compress(map1, custIm, 10);
 %figure(1); imshow(newPattern)
 fullPattern = patternCast(im, newPattern);
-
 %figure(1); imshow(fullPattern)
-gram = stereogram(im,fullPattern,15,5);
+gram = stereogram(im,fullPattern,25,2);
+%figure(2); imshow(gram)
+animate(imgaussfilt(fullPattern,2),gram,"picturePattern.gif");
+%animate(fullPattern,gram,"picturePattern.gif");
 
-figure(2); imshow(gram)
-animate(imgaussfilt(fullPattern,5),gram,"picturePattern.gif");
+%-------------------------------------------
+shark = im2double(imread("shark.png"));
+pattern = im2double(imread("pattern2.jpeg"));
+fullPattern2 = patternCast(shark,pattern);
+sharkGram = stereogram(shark,fullPattern2,15,2);
+imwrite(sharkGram,'sharkGram.jpeg')
+
+
 
 function depthMap = makeWordDepthMap(word,s) 
     alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
